e68996bd4e257da8ff9dfed4ff947378c0abe700
Derrick Dwamena <86938105+dwamenad@users.noreply.github.com>
2026-03-05 04:53:37 -0500

    build(extension): add bundled background worker output

---
+++ extension/dist/background.js
@@ -0,0 +1,25 @@
+// extension/src/background.ts
+var PUBMED_ORIGIN = "https://pubmed.ncbi.nlm.nih.gov";
+async function updateSidePanelForTab(tabId, url) {
+  if (!url) {
+    await chrome.sidePanel.setOptions({ tabId, enabled: false });
+    return;
+  }
+  const origin = new URL(url).origin;
+  await chrome.sidePanel.setOptions({
+    tabId,
+    path: "sidepanel.html",
+    enabled: origin === PUBMED_ORIGIN
+  });
+}
+chrome.runtime.onInstalled.addListener(() => {
+  void chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+});
+chrome.tabs.onUpdated.addListener((tabId, _changeInfo, tab) => {
+  void updateSidePanelForTab(tabId, tab.url);
+});
+chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+  const tab = await chrome.tabs.get(tabId);
+  void updateSidePanelForTab(tabId, tab.url);
+});
+//# sourceMappingURL=background.js.map
